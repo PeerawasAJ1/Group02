6003b2ea02716e0947bef7810510686a76b519c3
PeerawasAJ1 <67050161@go.buu.ac.th>
2025-07-23 16:56:15 +0700

    Add files via upload

---
+++ Loop/for_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  	// for loop to print "Hi" 5 times
+    for (int i = 0; i < 5; i++) { //set i=0;condition i<5; i = i+1 (loop for 5 time start with 0)
+      	cout << "Hi => i = " << i << endl; // show text + i if condition is TRUE
+    }
+  
+    return 0;
+}
---
+++ Loop/while_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  	// while loop to print numbers from 1 to 5
+    int i = 0; // Variable i=0
+    while (i < 5) { //condition i < 5 mean while i<5 do below code
+        cout << "Hi  => i = " << i << endl; // Show text + i
+        i++; // i = i+1
+    }
+    //while (true) {
+      //  cout<< "Hi => 1 =" << i << endl;
+        //i++;
+        
+    //}
+
+    return 0;
+}
